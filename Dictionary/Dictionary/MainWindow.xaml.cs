5b238a09ada67e861820b6a6e0c833e93812e0a5
Anya Webb <9946386@student.toiohomai.ac.nz>
2020-05-08 11:47:30 +1200

    Dictionary

--- Dictionary/Dictionary/MainWindow.xaml.cs
+++ Dictionary/Dictionary/MainWindow.xaml.cs
@@ -29,7 +29,7 @@ namespace Dictionary
                 "Cosmopolitan", new Drink()
                 {
                     Name = "Cosmopolitan",
-                    ImageName = "Images/Cosmopolitan.png",
+                    ImageName = "Images/cosmopolitan.jpg",
                     Recipe = "1.5 ounces of citrus vodka, 1 ounce of Cointreau, half an ounce of lime juice and a quarter of an ounce of cranberry juice.",
                     Mix = "Put all the ingredients in a shaker with ice and shake. Strain into a martini glass and garnish with a lime wheel or zest."
                 }
@@ -39,7 +39,7 @@ namespace Dictionary
                 "Daiquiri", new Drink()
                 {
                     Name = "Daiquiri",
-                    ImageName = "Images/Daiquiri.png",
+                    ImageName = "Images/daiquiri.jpg",
                     Recipe = "2 ounces of light rum, 1 ounce of simple syrup and 1 ounces of lime juice.",
                     Mix = "Shake ingredients with ice and strain into a cocktail glass. Garnish with a lime wheel."
                 }
@@ -49,7 +49,7 @@ namespace Dictionary
                 "Dark n Stormy", new Drink()
                 {
                     Name = "Dark n Stormy",
-                    ImageName = "Images/Dark n Stormy.png",
+                    ImageName = "Images/DarknStormy.jpg",
                     Recipe = "1.5 ounces of Gosling’s Black Seal Rum and ginger beer to top.",
                     Mix = "Fill a highball glass with ice and add rum and ginger beer. Garnish with lime."
                 }
@@ -59,7 +59,7 @@ namespace Dictionary
                 "French 75", new Drink()
                 {
                     Name = "French 75",
-                    ImageName = "Images/French 75.png",
+                    ImageName = "Images/french75.jpg",
                     Recipe = "2 ounces of gin, 2 dashes of simple syrup, half an ounce of lemon juice and champagne.",
                     Mix = "Shake gin, simple syrup, and lemon juice with ice. Strain into a champagne glass. Top with champagne."
                 }
@@ -69,7 +69,7 @@ namespace Dictionary
                 "Gimlet", new Drink()
                 {
                     Name = "Gimlet",
-                    ImageName = "Images/Gimlet.png",
+                    ImageName = "Images/gimlet.jpg",
                     Recipe = "2 ounces of gin or vodka, three quarters of an ounce of simple syrup, three quarters of an ounce of lime juice.",
                     Mix = "Shake ingredients with ice and strain into a cocktail glass."
                 }
@@ -79,7 +79,7 @@ namespace Dictionary
                 "Manhattan", new Drink()
                 {
                     Name = "Manhattan",
-                    ImageName = "Images/Manhattan.png",
+                    ImageName = "Images/manhattan.jpg",
                     Recipe = "2 ounces of silver tequila, 1 ounce of Cointreau, 1 ounce of lime juice and salt for the glass rim.",
                     Mix = "It should be shaken, then serve over ice in a glass with a salted rim."
                 }
@@ -89,7 +89,7 @@ namespace Dictionary
                 "Martinez", new Drink()
                 {
                     Name = "Martinez",
-                    ImageName = "Images/Martinez.png",
+                    ImageName = "Images/martinez.jpg",
                     Recipe = "1.5 ounces of Old Tom gin, 1.5 ounces of sweet vermouth, a quarter of an ounce of Luxardo maraschino liqueur and 2 dashes of Angostura or orange bitters.",
                     Mix = "Stir ingredients in a mixing glass with ice. Strain into chilled martini glass or cocktail glass."
                 }
@@ -99,7 +99,7 @@ namespace Dictionary
                 "Martini", new Drink()
                 {
                     Name = "Martini",
-                    ImageName = "Images/Martini.png",
+                    ImageName = "Images/martini.jpg",
                     Recipe = "3 ounces of gin or vodka, half an ounce of dry vermouth and lemon peel or an olive.",
                     Mix = "Stir ingredients in a mixing glass with ice. Strain into a chilled martini glass. Squeeze oil from the lemon peel into the glass or garnish with an olive."
                 }
@@ -109,7 +109,7 @@ namespace Dictionary
                 "Mimosa", new Drink()
                 {
                     Name = "Mimosa",
-                    ImageName = "Images/Mimosa.png",
+                    ImageName = "Images/mimosa.jpg",
                     Recipe = "2.5 ounces of champagne and 2.5 ounces of orange juice.",
                     Mix = "Combine equal parts of the ingredients in a champagne flute."
                 }
@@ -119,7 +119,7 @@ namespace Dictionary
                 "Mint Julep", new Drink()
                 {
                     Name = "Mint Julep",
-                    ImageName = "Images/Mint Julep.png",
+                    ImageName = "Images/mintJulep.jpg",
                     Recipe = "2 ounces of bourbon, 8 to 10 mint leaves and a quarter of an ounce of simple syrup.",
                     Mix = "Mix-up the mint leaves and simple syrup in a cup. Add bourbon and fill with crushed ice. Stir until the cup is frosted. Fill with more crushed ice. Serve with a straw and a mint sprig garnish."
                 }
@@ -129,7 +129,7 @@ namespace Dictionary
                 "Mojito", new Drink()
                 {
                     Name = "Mojito",
-                    ImageName = "Images/Mojito.png",
+                    ImageName = "Images/mojito.jpg",
                     Recipe = "2 ounces of vodka, 4 to 6 ounces of ginger beer and 5 ounces of lime juice.",
                     Mix = "Squeeze lime juice into a mug, add two or three ice cubes, pour in the vodka, and fill with cold ginger beer. Stir and serve."
                 }
@@ -139,7 +139,7 @@ namespace Dictionary
                 "Negroni", new Drink()
                 {
                     Name = "Negroni",
-                    ImageName = "Images/Negroni.png",
+                    ImageName = "Images/negroni.jpg",
                     Recipe = "1 ounce of gin, 1 ounce of Campari and 1 ounce of sweet vermouth.",
                     Mix = "Stir ingredients with ice."
                 }
@@ -149,7 +149,7 @@ namespace Dictionary
                 "Old Fashioned", new Drink()
                 {
                     Name = "Old Fashioned",
-                    ImageName = "Images/Old Fashioned.png",
+                    ImageName = "Images/oldFashioned.jpg",
                     Recipe = "2 ounces of bourbon or rye whiskey, 2 dashes of Angostura bitters, 1 sugar cube or 1 tablespoon sugar and an orange twist garnish.",
                     Mix = "Put sugar in glass. Cover it with dashes of bitters. Add whiskey and stir until the sugar dissolves. Add ice, stir again, and serve."
                 }
@@ -159,7 +159,7 @@ namespace Dictionary
                 "Paloma", new Drink()
                 {
                     Name = "Paloma",
-                    ImageName = "Images/Paloma.png",
+                    ImageName = "Images/palmona.jpg",
                     Recipe = "2 ounces of tequila, half an ounce of lime juice and grapefruit soda to top.",
                     Mix = "Add tequila and lime to a salt-rimmed glass filled with ice. Top with grapefruit soda"
                 }
@@ -169,7 +169,7 @@ namespace Dictionary
                 "Pimms Cup", new Drink()
                 {
                     Name = "Pimms Cup",
-                    ImageName = "Images/Pimms Cup.png",
+                    ImageName = "Images/pimmsCup.jpg",
                     Recipe = "1.75 ounces of  Pimm's No.1, 5 ounces of lemonade, mint, orange, strawberries and cucumber to garnish.",
                     Mix = "Pile all the ingredients in a tall glass, mix, and sip."
                 }
@@ -179,7 +179,7 @@ namespace Dictionary
                 "Pina Colada", new Drink()
                 {
                     Name = "Pina Colada",
-                    ImageName = "Images/Pina Colada.png",
+                    ImageName = "Images/pinaColada.jpg",
                     Recipe = "3 ounces of white rum, 1.5 ounces of coconut cream, 3.5 ounces of pineapple juice, 10 ounces of crushed ice and 1 slice of pineapple.",
                     Mix = "Place the crushed ice into a blender and whizz it for 15 seconds. Now add the coconut cream, rum and the pineapple juice. Blend it on medium until you get a smooth, creamy mixture. Pour it into a tall wine glass and garnish the side of the glass with a slice of pineapple."
                 }
@@ -189,7 +189,7 @@ namespace Dictionary
                 "Sazerac", new Drink()
                 {
                     Name = "Sazerac",
-                    ImageName = "Images/Sazerac.png",
+                    ImageName = "Images/sazerac.jpg",
                     Recipe = "2 ounces of rye whiskey, half an ounce of simple syrup, 2 dashes of Peychaud's bitters and Absinthe.",
                     Mix = "Rinse a chilled glass with absinthe and discard the absinthe. Stir the other ingredients in a mixing glass, strain into the chilled glass, and garnish."
                 }
@@ -199,7 +199,7 @@ namespace Dictionary
                 "Sidecar", new Drink()
                 {
                     Name = "Sidecar",
-                    ImageName = "Images/Sidecar.png",
+                    ImageName = "Images/sideCar.jpg",
                     Recipe = "2 ounces of VS or VSOP Cognac, 1 ounce of Cointreau, three quarters of an ounce of lemon juice.",
                     Mix = "Shake ingredients with ice. Strain into a rocks glass or a cocktail class with a sugar-coated rim."
                 }
@@ -209,7 +209,7 @@ namespace Dictionary
                 "Singapore Sling", new Drink()
                 {
                     Name = "Singapore Sling",
-                    ImageName = "Images/Singapore Sling.png",
+                    ImageName = "Images/singaporeSling.jpg",
                     Recipe = "1.5 ounces of Ford’s Gin, a quarter of an ounce of Cherry Herring, a quarter of an ounce of Combier, a quarter of an ounce of Benedictine, 2 ounces  of pineapple juice and a quarter of an ounce of grenadine.",
                     Mix = "Add all the ingredients into a shaker over ice. Shake and strain into tiki glass with fresh pebble ice. Garnish with orange peel and macerated cherry."
                 }
@@ -219,7 +219,7 @@ namespace Dictionary
                 "Surfer Girl", new Drink()
                 {
                     Name = "Surfer Girl",
-                    ImageName = "Images/Surfer Girl.png",
+                    ImageName = "Images/surferGirl.jpg",
                     Recipe = "1.5 ounces of Four Pillars Rare Dry Gin, half an ounce of lime juice, half an ounce of honey, 1 ounce of watermelon juice and 5 to 6 basil leaves.",
                     Mix = "Combine all ingredients in a shaker over ice. Shake. Strain into a coupe glass and garnish with basil."
                 }
@@ -229,7 +229,7 @@ namespace Dictionary
                 "Whiskey", new Drink()
                 {
                     Name = "Whiskey",
-                    ImageName = "Images/Whiskey Sour.png",
+                    ImageName = "Images/whiskeySour.jpg",
                     Recipe = "2 ounces of whiskey, 1 ounce of lemon juice, 1 tablespoon of sugar and 1 egg white optional.",
                     Mix = "Combine ingredients in a cocktail shaker and shake. Add ice and shake again. Strain over ice in a rocks glass."
                 }
